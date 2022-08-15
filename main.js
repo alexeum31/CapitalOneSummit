1720fbfe7db5b5d7cb06e51473429c2363a81a54
Alexander <ae2576@princeton.edi>
2022-08-15 15:38:49 -0400

    js- works

--- main.js
+++ main.js
@@ -9,42 +9,46 @@ function retrieve(e){
 
   e.preventDefault();
   let videoURL = input.value;
-  const videoID = videoURL.split("v=");
+  let videoID = videoURL.split("v=")[1]
+  console.log ("videoID", videoID)
+  callAPI(videoID)
 }
 //api URL creation
 
-const apiData= { 
-    url: "https://subtitles-for-youtube.p.rapidapi.com",
-    slash: "/",
-    id: document.querySelector(videoID)
-};
+// const apiData= { 
+    // url: "https://subtitles-for-youtube.p.rapidapi.com",
+//     slash: "/",
+//     id: videoID
+// };
 
-const {url, slash, id} = apiData;
-const apiURL= `${URL}${slash}${id}`;
+// const {url, slash, id} = apiData;
+// const apiURL= `${URL}${slash}${id}`;
 
 //activate API call
+function callAPI(videoID) {
+  let apiURL= `https://subtitles-for-youtube.p.rapidapi.com/subtitles/${videoID}`;
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'f2b0ea9532msh4a7c440a376bc0cp1ef3e0jsne053e437c41f',
-		'X-RapidAPI-Host': 'subtitles-for-youtube.p.rapidapi.com'
-	}
-};
-
-fetch(apiURL, options)
-.then(response => response.json())
-  .then(response => 
-  {response.forEach(
-    (subtext.text) => {
-      let D = document.createElement("p"),
-      let a = document.createElement("p"),
-      a.textContent = subtext.text,
-      D.className = "subtitles",
-      D.appendChild(a),
-      subtitles.appendChild(D),
-      
-    })})
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'f2b0ea9532msh4a7c440a376bc0cp1ef3e0jsne053e437c41f',
+      'X-RapidAPI-Host': 'subtitles-for-youtube.p.rapidapi.com'
+    }
+  };
+   
+  fetch(apiURL, options)
+  .then(response => response.json())
+    .then(response => 
+    {response.forEach(
+      (subtext) => {
+        let D = document.createElement("p");
+        let a = document.createElement("p");
+        a.textContent = subtext.text;
+        D.className = "subtitles";
+        D.appendChild(a);
+        subtitles.appendChild(D);
+        
+      })})}
 
 
 
@@ -56,3 +60,7 @@ fetch(apiURL, options)
 //set it to a variable, have that variable connect to the constant. 
 //in my fetch it will take in parts of the url and the id. 
 //function called on a search, difference is take the string to get the last part and set that as the constant.
+
+//right now, fetch isnt a function. its just being ran as soon as the file loaded which is everytime i refresh
+//i want it to be called after the input value. wrap fetch in the function and call the function
+//write a function and put the fetch stuff in there
